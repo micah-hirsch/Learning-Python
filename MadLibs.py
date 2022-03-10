a3b49142b546c15bf184977c69145af2875c5329
micah-hirsch <micahhirsch06@gmail.com>
2022-03-10 15:30:44 -0500

    Beginning MadLibs exercise

---
+++ MadLibs.py
@@ -0,0 +1,10 @@
+# This is a test project creating a Mad Libs game. This is based on the Youtube tutorial by freeCodeCamp.org. Tutorial by Kylie Ying.
+# Link to Youtube tutorial: https://youtu.be/8ext9G7xspg
+
+# String Concatenation
+youtuber = "" # Some String
+
+# A few ways to do this
+print("subscribe to " + youtuber)
+print("subscribe to {} ".format(youtuber))
+print(f "subscribe to {youtuber}")
