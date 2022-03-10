c8fe3224b3e89bba5aff2ceef031e1acdea0de2b
micah-hirsch <micahhirsch06@gmail.com>
2022-03-10 16:13:00 -0500

    Guess the number game created

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,3 +1,16 @@
 # Guess the number coding tutorial from freeCodeCamp.org instucted by Kylie Ying.
 # Youtube link: https://youtu.be/8ext9G7xspg
 
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Guess a number between 1 and {x}: "))
+        if guess < random_number:
+            print("Sorry, guess again. Too low.")
+        elif guess > random_number:
+            print("Sorry, guess again. Too high.")
+    print(f"Yay! Congrats! You have guessed the number {random_number} correctly")
+guess(10)
