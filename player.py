ba01f19a6071458e115b52f1e79ec016732b5053
micah-hirsch <micahhirsch06@gmail.com>
2022-03-12 14:56:17 -0500

    Tic Tac Toe Game: Currently not working

---
+++ player.py
@@ -0,0 +1,42 @@
+# player for tic-tac-toe game
+
+import math
+import random
+
+class Player:
+    def __init__(self, letter):
+        self.letter = letter # letter is x or o
+
+    #all players get their next move
+
+    def get_move(self, game):
+        pass
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + "'s turn. Input move (0-8): ")
+            try:
+                val = int(square)
+                if value not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print("Invalid Square. Try Again.")
+
+        return val
+
+
