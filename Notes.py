f7768625679fd26ec3aa2ac390ea3587c1628390
micah-hirsch <micahhirsch06@gmail.com>
2022-03-14 21:16:27 -0400

    Continued youtube tutorial on Python basics

--- Notes.py
+++ Notes.py
@@ -5,6 +5,10 @@
 
 # Variables and Data Types
 
+#importing python math
+
+from math import *
+
 ## Strings
 
 character_name = "George" # Just change variable to change story below
@@ -47,3 +51,65 @@ print(phrase.index("o")) # index function. Passing a parameter. finding position
 print(phrase.replace("Disorders", "Conditions")) # replace function
 
 # Working with numbers
+
+print(2) # Just printing the number integer
+print(2.09) # float
+print(3 + 4.5) # addition, but also subtraction, multiplication, division, ... etc.
+print(10 % 3) # modulus operator gives you the remainder from 10/3
+my_num = 5
+print(my_num) # storing number variable then printing it out
+print(str(my_num)) #converting into a string
+
+## Functions (Not an exhaustive list)
+print(abs(my_num)) # absolute value of the number
+print(pow(10, 3)) # 10 ^ 3 "to the power"
+print(max(10, 3)) # return the largest number
+print(min(10, 3)) # print out the smallest number
+print(round(3.2)) # rounding a number according to typical rounding rules
+print(floor(3.7)) # getting the lowest number
+print(ceil(3.2)) # rounding number up (i.e. 4)
+print(sqrt(36)) # square root function
+
+# Getting Input from Users
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(f"Hi {name}! You are {age} years old.")
+
+# Lists
+
+friends = ["Austin", "Rowan", "Cassie"]
+print(friends)
+print(friends[1]) # Getting item in second position of the list
+print(friends[-1]) # starts from the back of the list
+print(friends[1:]) # indexing a range of names
+print(friends[0:1])
+friends[2] = "Stephanie" # modifying values in list. This will replace "Cassie" with "Stephanie"
+
+# List Functions
+
+lucky_numbers = [4, 8, 15, 16, 25, 30]
+friends.extend(lucky_numbers) # appending lucky numbers list on the friends list
+friends.append("Caitlin")# Adding an additional item to end of list
+friends.insert(1, "Alyssa") # Adding an additional item at index position 1 (second item)
+friends.remove("Austin") #removing Austin from the freinds list
+friends.clear() #empties the list
+friends.pop() # gets rid of the last element in the list
+print(friends.index("Rowan")) # is Rowan in the list?
+print(friends.count("Austin")) # count number of times an element appears in the list
+friends.sort() # sort list in ascending order
+lucky_numbers.sort() # ascending order
+lucky_numbers.reverse() # reverse the order of the list
+friends2 = friends.copy() # copying a list
+
+# Tuples
+
+coordinates = (4, 5) #tuple with coordinates
+
+## Tubles are immutable. They cannot be changed or modified once created.
+
+print(coordinates[1]) # index
+
+list_coordinates = [(4, 5), (6, 7), (8, 9)] # a list of tuples
+
+# Functions
